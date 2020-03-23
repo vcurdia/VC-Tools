1a6d280021249c33918f72fd8230dd16d7da28df
Vasco Curdia <vcurdia@gmail.com>
2020-03-23 13:21:28 -0700

    fix typo in progress

--- progress.m
+++ progress.m
@@ -1,4 +1,4 @@
-function progress(j,n,nblocks)
+function progress(j,n,varargin)
 % vcprogress
 % 
 % Track progress of job at step j out of n.
@@ -6,11 +6,14 @@ function progress(j,n,nblocks)
 % Created: March 23, 2020
 % Copyright 2020 by Vasco Curdia
 
-    if nargin<3 || isempty(nblocks)
-        nblocks = 10;
-    end
-    if ~mod(j,ceil(n/nblocks)) || j==n
-        fprintf('completed %3.0f%%\n',j/n*100)
+    op.nblocks = 10;
+    op.fid = 1;
+    op = updateoptions(op,varargin{:});
+
+    if ~mod(j,ceil(n/op.nblocks)) || j==n
+        fprintf(op.fid,'completed %3.0f%%\n',j/n*100);
     end
 
 end
+
+
