ccfafa926f5f9bd577fc2eb24d8b0bb5e7d12c74
Vasco Curdia <vasco.curdia@sf.frb.org>
2023-06-29 00:27:03 -0700

    savecsv

---
+++ savecsv.m
@@ -0,0 +1,25 @@
+function savecsv(fn,var,tid,data)
+
+    fidcsv = fopen([fn,'.csv'],'wt');
+    [ntid,nvar] = size(data);
+
+    if ~iscell(tid)
+        tid = num2cell(tid);
+    end
+    for t=1:ntid
+        if ~ischar(tid{t})
+            tid{t} = num2str(tid{t});
+        end
+    end
+
+    fprintf(fidcsv,', %s',var{:});
+    fprintf(fidcsv,'\n');
+    for t=1:ntid
+        fprintf(fidcsv,'%s',tid{t});
+        fprintf(fidcsv,', %.6f',data(t,:));
+        fprintf(fidcsv,'\n');
+    end
+    
+    fclose(fidcsv);
+
+end
