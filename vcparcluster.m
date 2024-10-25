d9bd2a035cc230b95419026025ce393a8645b5bf
Vasco Curdia <vasco.curdia@sf.frb.org>
2024-10-25 21:59:41 +0000

    vcparcluster takes optional input for nworkers

--- vcparcluster.m
+++ vcparcluster.m
@@ -1,4 +1,4 @@
-function [c,cbatch] = vcparcluster(cname)
+function c = vcparcluster(cname,nworkers)
 % vcparcluster
 % 
 % save copy of default cluster profile to  make one per machine
@@ -10,10 +10,15 @@ function [c,cbatch] = vcparcluster(cname)
     ps.Pool.IdleTimeout = inf;
 
     c = parcluster;
-    c.NumWorkers = 64;
+    
+    if nargin>1
+        c.NumWorkers = nworkers;
+    end
+
     c.JobStorageLocation = checkdir([c.JobStorageLocation,'/',cname]);
+
     c.saveAsProfile(cname)
 
-    cbatch = @(x,n)c.batch(x,0,{},'Pool',n,'AutoAttachFiles',false);
+    % cbatch = @(x,n)c.batch(x,0,{},'Pool',n,'AutoAttachFiles',false);
 
 end
